d8b3e0365cf57211dad9668c709cfe97c1759121
t0ux <wandrease@gmail.com>
2022-05-06 23:40:31 +0300

    overflow again and titles

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -5,50 +5,20 @@ import Footer from '@components/footer/footer.component';
 import Navbar from '@components/navbar.component';
 import { ChatAltIcon } from '@heroicons/react/outline';
 import { motion } from 'framer-motion';
+import Head from 'next/head';
 
 const ContactPage = () => {
   return (
-    <div className={styles.bodyWrapper}>
-      <Navbar />
-      <NavbarMobile />
-      <div className={styles.contact_container}>
-        <motion.div
-          className={styles.contact_container_hero}
-          initial={{ opacity: 0, x: -150, scale: 0.98 }}
-          animate={{
-            opacity: 1,
-            x: 0,
-            scale: 1,
-            transition: {
-              duration: 0.25,
-              type: 'tween',
-            },
-          }}
-        >
-          <h1>Contact Me</h1>
-          <p>Want to work together? Just get in touch below!</p>
-        </motion.div>
-        <div className={styles.contact_container_body}>
-          <motion.p
-            className={styles.contact_container_body_email}
-            initial={{ opacity: 0, x: 150, scale: 0.98 }}
-            animate={{
-              opacity: 1,
-              x: 0,
-              scale: 1,
-              transition: {
-                duration: 0.25,
-                type: 'tween',
-              },
-            }}
-          >
-            <span>
-              <ChatAltIcon className={styles.atsymbol} /> Email: &nbsp;
-            </span>
-            rosenthalmusic@gmail.com
-          </motion.p>
+    <>
+      <Head>
+        <title>Rosenthal Music | Contact</title>
+      </Head>
+      <div className={styles.bodyWrapper}>
+        <Navbar />
+        <NavbarMobile />
+        <div className={styles.contact_container}>
           <motion.div
-            className={styles.contact_container_body_alt}
+            className={styles.contact_container_hero}
             initial={{ opacity: 0, x: -150, scale: 0.98 }}
             animate={{
               opacity: 1,
@@ -60,16 +30,52 @@ const ContactPage = () => {
               },
             }}
           >
-            <p>or you can find me on... </p>
-            <div>
-              <a href="https://instagram.com/rosenthal">Instagram</a>
-              <a href="https://instagram.com/rosenthal">LinkedIn</a>
-            </div>
+            <h1>Contact Me</h1>
+            <p>Want to work together? Just get in touch below!</p>
           </motion.div>
+          <div className={styles.contact_container_body}>
+            <motion.p
+              className={styles.contact_container_body_email}
+              initial={{ opacity: 0, x: 150, scale: 0.98 }}
+              animate={{
+                opacity: 1,
+                x: 0,
+                scale: 1,
+                transition: {
+                  duration: 0.25,
+                  type: 'tween',
+                },
+              }}
+            >
+              <span>
+                <ChatAltIcon className={styles.atsymbol} /> Email: &nbsp;
+              </span>
+              rosenthalmusic@gmail.com
+            </motion.p>
+            <motion.div
+              className={styles.contact_container_body_alt}
+              initial={{ opacity: 0, x: -150, scale: 0.98 }}
+              animate={{
+                opacity: 1,
+                x: 0,
+                scale: 1,
+                transition: {
+                  duration: 0.25,
+                  type: 'tween',
+                },
+              }}
+            >
+              <p>or you can find me on... </p>
+              <div>
+                <a href="https://instagram.com/rosenthal">Instagram</a>
+                <a href="https://instagram.com/rosenthal">LinkedIn</a>
+              </div>
+            </motion.div>
+          </div>
         </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </>
   );
 };
 
--- pages/work/index.tsx
+++ pages/work/index.tsx
@@ -10,6 +10,7 @@ import Navbar from '@components/navbar.component';
 import Footer from '@components/footer/footer.component';
 import { AnimatePresence, motion } from 'framer-motion';
 import NavbarMobile from '@components/navbar_mobile/navbar_mobile.component';
+import Head from 'next/head';
 
 const container = {
   hidden: { opacity: 0, scale: 0.95 },
@@ -22,51 +23,55 @@ const container = {
   },
 };
 
-const child = {
-  hidden: { opacity: 0 },
-  show: { opacity: 1 },
-};
-
 function WorkPage() {
   const { data } = useQueryDataCollection('work');
 
   return (
-    <section className={styles.bodyWrapper}>
-      <Navbar />
-      <NavbarMobile />
-      <div className={styles.contentWrapper}>
-        <h1>Discography</h1>
-        <motion.section
-          className={styles.cards}
-          variants={container}
-          initial="hidden"
-          animate="show"
-        >
-          <AnimatePresence exitBeforeEnter>
-            {data?.items.map((item) => {
-              return (
-                <Link href={`/work/${item.sys.id}`} passHref key={item.sys.id}>
-                  <motion.div
-                    className={styles.cards__cardImage}
-                    data-song-name={item?.fields.songTitle}
-                    whileTap={{ scale: 1.05 }}
+    <>
+      <Head>
+        <title>Rosenthal Music | Discography</title>
+      </Head>
+      <section className={styles.bodyWrapper}>
+        <Navbar />
+        <NavbarMobile />
+        <div className={styles.contentWrapper}>
+          <h1>Discography</h1>
+          <motion.section
+            className={styles.cards}
+            variants={container}
+            initial="hidden"
+            animate="show"
+          >
+            <AnimatePresence exitBeforeEnter>
+              {data?.items.map((item) => {
+                return (
+                  <Link
+                    href={`/work/${item.sys.id}`}
+                    passHref
+                    key={item.sys.id}
                   >
-                    <Image
-                      alt="song or album art cover"
-                      src={`https:${item.fields.songCover?.fields.file.url}`}
-                      layout="responsive"
-                      width={300}
-                      height={300}
-                    />
-                  </motion.div>
-                </Link>
-              );
-            })}
-          </AnimatePresence>
-        </motion.section>
-      </div>
-      <Footer />
-    </section>
+                    <motion.div
+                      className={styles.cards__cardImage}
+                      data-song-name={item?.fields.songTitle}
+                      whileTap={{ scale: 1.05 }}
+                    >
+                      <Image
+                        alt="song or album art cover"
+                        src={`https:${item.fields.songCover?.fields.file.url}`}
+                        layout="responsive"
+                        width={300}
+                        height={300}
+                      />
+                    </motion.div>
+                  </Link>
+                );
+              })}
+            </AnimatePresence>
+          </motion.section>
+        </div>
+        <Footer />
+      </section>
+    </>
   );
 }
 
