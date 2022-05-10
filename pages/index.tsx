2a4ae7b313718026777048665c40704e338fb8f7
t0ux <wandrease@gmail.com>
2022-05-10 14:38:36 +0300

    meta title bugfix(??)

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,21 +9,25 @@ import SEO from '@components/SEO/seo.component';
 //styles + icons
 import styles from '@styles/index_page.module.scss';
 import { useQueryDataCollection } from '@hooks/useQueryData';
+import Head from 'next/head';
 
 const Home: NextPage = () => {
   const { data } = useQueryDataCollection<'homepage'>('homepage');
-  const imageAbout = `https:${data?.items[0]?.fields.image.fields.file.url}`;
+  // const imageAbout = `https:${data?.items[0]?.fields.image.fields.file.url}`;
 
   return (
     <div className={styles.index}>
-      {data !== undefined && (
-        <SEO
-          title="Home"
-          description="Music producer, mixing & mastering engineer based in Estonia."
-          image={imageAbout}
+      <Head>
+        <title>Rosenthal Music | Home</title>
+        <meta
+          name="description"
+          content="Music producer, mixing & mastering engineer from Estonia."
         />
-      )}
-
+        <meta name="name" content="Sander Rosenthal" />
+        <meta name="image" content="../public/roseicon.svg" />
+        <meta name="robots" content="index, nofollow" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
       <section className={styles.bodyWrapper}>
         <Navbar />
         <NavbarMobile />
